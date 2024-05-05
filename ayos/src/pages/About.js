e468b92b2eeb220678a88dcee4fec72ab315ec47
Alyssa Karina <yaneza@email.arizona.edu>
2024-05-05 10:12:30 -0700

    added basic about info

--- ayos/src/pages/About.js
+++ ayos/src/pages/About.js
@@ -1,3 +1,9 @@
 export default function About() {
-    return <h1>About</h1>
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is some additional text about the About page.</p>
+            <p>You can add as much text as you like here.</p>
+        </div>
+    );
 }
